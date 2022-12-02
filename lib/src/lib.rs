c34e890eaf754b3f8d5d21dc1624dae7940d370c
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2022-12-01 23:40:42 -0500

    lib for input parsing in rust

---
+++ lib/src/lib.rs
@@ -0,0 +1,21 @@
+use std::io::BufReader;
+use std::fs::File;
+use std::path::Path;
+use std::io::BufRead;
+
+pub fn read_input(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+pub fn read_input_i32(filename: impl AsRef<Path>) -> Vec<i32> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .map(|l| l.parse::<i32>().expect("Could not parse int"))
+        .collect()
+}
---
+++ lib/src/math/math.rs
@@ -0,0 +1,5 @@
+pub mod math {
+    pub fn ceil(x: i32) -> i32 {
+        (x as f64).ceil()
+    }
+}
