ef9a7793b1b6a84df9b1a3ecc3c1f4a59810b3fd
Charles Bourget <cbourget@stingray.com>
2022-12-09 08:52:11 -0500

    hole in one

--- extras/day06.py
+++ extras/day06.py
@@ -1,4 +1,1 @@
-line = open("../inputs/day06/input.txt").read().strip()
-def calculate(length):
-    return [i+length for i in range(len(line)) if len(set(line[i:i+length]))==length][0]
-print(f"Part 1 {calculate(4)}\nPart 2 {calculate(14)}")
+print(f"Part 1 {[i+4 for i in range(len(open('../inputs/day06/input.txt').read().strip())) if len(set(open('../inputs/day06/input.txt').read().strip()[i:i+4]))==4][0]}\nPart 2 {[i+14 for i in range(len(open('../inputs/day06/input.txt').read().strip())) if len(set(open('../inputs/day06/input.txt').read().strip()[i:i+14]))==14][0]}")
