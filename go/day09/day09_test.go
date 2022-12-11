b035c06b01869209a2aa681bf90ee3f5ed0caadd
Charles Bourget <charles.bourget.2@ens.etsmtl.ca>
2022-12-10 20:45:24 -0500

    day 09

---
+++ go/day09/day09_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/charlesbourget/aoc2022/lib"
+)
+
+func TestPart1(t *testing.T) {
+	input, err := lib.Read("../../inputs/day09/input.test")
+	if err != nil {
+		panic(fmt.Errorf("Error while reading input, %s", err))
+	}
+
+	var expected int = 13
+	var result int = Part1(input)
+	if result != expected {
+		t.Fatalf(`Part1() = %d, want %d, error`, result, expected)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input, err := lib.Read("../../inputs/day09/input.2.test")
+	if err != nil {
+		panic(fmt.Errorf("Error while reading input, %s", err))
+	}
+
+	var expected int = 36
+	var result int = Part2(input)
+	if result != expected {
+		t.Fatalf(`Part2() = %d, want %d, error`, result, expected)
+	}
+}
